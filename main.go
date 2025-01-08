5a7a28df5e328ebf740db9902ae08ebb46acd79a
HeoSeokMun <tjrans303@soongsil.ac.kr>
2025-01-08 23:24:46 +0900

    축약

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 import "gojob/something"
 
+var name string = "seokmun"
+
 func main() {
+
+	n := "sm"
+
 	fmt.Println("Hello World")
 
 	something.SayBye()
 	something.SayHello()
+
+	fmt.Println(name)
+	fmt.Println(n)
 }
