ffae6137f327145d93074dab39403c0f07373cca
HeoSeokMun <tjrans303@soongsil.ac.kr>
2025-02-02 15:15:28 +0900

    slice

--- main.go
+++ main.go
@@ -8,4 +8,13 @@ func main() {
 
 	*b = 5
 	fmt.Println(*b)
+
+	slice := []int{1, 2, 3}
+	fmt.Println(slice)
+
+	arr := [5]string{"a", "b", "c"}
+	fmt.Println(arr)
+
+	slice = append(slice, 4)
+	fmt.Println(slice)
 }
